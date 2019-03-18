dca974314240eba11e29bb3359c7d9bffca4a3a8
Anton Nebykov <nebykoff@gmail.com>
2019-03-18 11:36:22 +0700

    Удалено лишнее

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
-const watch = require('gulp-watch');
 
 gulp.task('sass-compile', function(){
     return gulp.src('./scss/**/*.scss')
