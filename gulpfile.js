540c8edd0ec67427492c6bb8ae81aae3d0371997
Anton Nebykov <nebykoff@gmail.com>
2019-03-22 17:12:22 +0700

    Добавлен список браузеров autoprefixerList

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,23 @@ const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 
+var autoprefixerList = [
+    'Chrome >= 45',
+    'Firefox ESR',
+    'Edge >= 12',
+    'Explorer >= 10',
+    'iOS >= 9',
+    'Safari >= 9',
+    'Android >= 4.4',
+    'Opera >= 30'
+];
+
 gulp.task('build:css', function () {
     return gulp.src('./scss/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
         .pipe(autoprefixer({
-            browsers: ['last 2 versions']            
+            browsers: autoprefixerList            
         }))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./css/'))
