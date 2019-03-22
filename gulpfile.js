d6ad8f0214585612b35b98c2efd10396482ccb66
Anton Nebykov <nebykoff@gmail.com>
2019-03-22 17:04:27 +0700

    Добавлен autoprefixer и расширеный вывод css

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('sass-compile', function(){
+gulp.task('build:css', function () {
     return gulp.src('./scss/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write('./'))    
-    .pipe(gulp.dest('./css/'))
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']            
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./css/'))
 })
 
-gulp.task('watch', function(){
-    gulp.watch('./scss/**/*.scss', gulp.series('sass-compile'))
+gulp.task('watch', function () {
+    gulp.watch('./scss/**/*.scss', gulp.series('build:css'))
 })
