081cd777a02feb27a1a6d03770017274ca987594
Anton Nebykov <nebykoff@gmail.com>
2019-04-10 10:14:35 +0700

    Добавлен smart-grid

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
+var gcmq = require('gulp-group-css-media-queries');
+var smartgrid = require('smart-grid');
+
+var stylesPath = './scss';
 
 var autoprefixerList = [
     'Chrome >= 45',
@@ -15,16 +19,59 @@ var autoprefixerList = [
 ];
 
 gulp.task('build:css', function () {
-    return gulp.src('./scss/**/*.scss')
+    return gulp.src(stylesPath+'/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: autoprefixerList            
         }))
+        .pipe(gcmq()) //Группировка медиа запросов
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./css/'))
 })
 
 gulp.task('watch', function () {
-    gulp.watch('./scss/**/*.scss', gulp.series('build:css'))
+    gulp.watch(stylesPath+'/**/*.scss', gulp.series('build:css'))
 })
+
+
+/* It's principal settings in smart grid project */
+var sgSettings = {
+    filename: '_smart-grid',
+    outputStyle: 'scss', /* less || scss || sass || styl */
+    columns: 12, /* number of grid columns */
+    offset: '30px', /* gutter width px || % || rem */
+    mobileFirst: false, /* mobileFirst ? 'min-width' : 'max-width' */
+    container: {
+        maxWidth: '1200px', /* max-width оn very large screen */
+        fields: '30px' /* side fields */
+    },
+    breakPoints: {
+        lg: {
+            width: '1100px', /* -> @media (max-width: 1100px) */
+        },
+        md: {
+            width: '960px'
+        },
+        sm: {
+            width: '780px'            
+        },
+        xs: {
+            width: '560px'
+        }
+        /* 
+        We can create any quantity of break points.
+
+        some_name: {
+            width: 'Npx',
+            fields: 'N(px|%|rem)',
+            offset: 'N(px|%|rem)'
+        }
+        */
+    }
+};
+
+gulp.task('smart-grid', function (cb) {
+    smartgrid(stylesPath, sgSettings);
+    cb();
+});
